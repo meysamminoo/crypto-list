7e290f26828951c355bd1d16a8d99112b18836c7
meysam <meisam.design.ph@gmail.com>
2022-03-17 12:13:39 +0330

    get data in api

---
+++ code.js
@@ -0,0 +1,7 @@
+fetch(
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+)
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result);
+  });
