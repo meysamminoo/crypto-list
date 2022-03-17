712c15821fb937883cbbf71dde509bf122c8b5dd
meysam <meisam.design.ph@gmail.com>
2022-03-17 12:46:00 +0330

    add image, name, symbol

--- code.js
+++ code.js
@@ -12,5 +12,25 @@ fetch(
 function addToDom(data) {
   const coinRow = document.createElement("div");
   coinRow.classList.add("coin-row");
+  // * add image
+  const imageElement = document.createElement("img");
+  imageElement.classList.add("coin-image");
+  imageElement.setAttribute("src", data.image);
+  imageElement.setAttribute("alt", data.name);
+  coinRow.appendChild(imageElement);
+
+  // * add name
+  const nameElement = document.createElement("h2");
+  nameElement.innerText = data.name;
+  coinRow.appendChild(nameElement);
+
+  // * add symbol
+  const symbolElement = document.createElement("p");
+  symbolElement.classList.add("coin-symbol");
+  symbolElement.innerText = data.symbol;
+  coinRow.appendChild(symbolElement);
+
+  // * add price data
+
   coinContainer.appendChild(coinRow);
 }
