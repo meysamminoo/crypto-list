e9490d4231b839bca52aec4fa4409d03800cc988
meysam <meisam.design.ph@gmail.com>
2022-03-17 12:30:27 +0330

    create coin row

--- code.js
+++ code.js
@@ -1,7 +1,16 @@
+// ! Selectors
+const coinContainer = document.querySelector(".coin-container");
+
 fetch(
   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
 )
   .then((response) => response.json())
   .then((result) => {
-    console.log(result);
+    result.map((data) => addToDom(data));
   });
+
+function addToDom(data) {
+  const coinRow = document.createElement("div");
+  coinRow.classList.add("coin-row");
+  coinContainer.appendChild(coinRow);
+}
